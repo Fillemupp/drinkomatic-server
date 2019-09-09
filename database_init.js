e37c744014394614d786df81cfc49105a4a0b0e6
Jimmy Eiterjord <jimmy.eiterjord@gmail.com>
2019-09-10 00:59:46 +0100

    Added new drinks to database init

--- database_init.js
+++ database_init.js
@@ -42,18 +42,18 @@ var data = [
 		   { "motor" : "21", "mixer" : "fanta", "stepspercl" : "3579" },
 		   { "motor" : "22", "mixer" : "orange", "stepspercl" : "3579" },
 		   { "motor" : "23", "mixer" : "grenadine", "stepspercl" : "3579" } ] },
-    
+
     { "_id" : ObjectId("5bcb8e5c313670bd53953d7f"),
       "type" : "drink",
       "name" : "Rom & Cola", "sysname" : "romcola",
       "mixers" : [ { "mixer" : "rom", "amount" : "4" },
 		   { "mixer" : "cola", "amount" : "16" } ] },
-    
+
     { "_id" : ObjectId("5bcb8e6c313670bd53953d80"),
       "type" : "drink",
       "name" : "Gin & Tonic", "sysname" : "gintonic",
       "mixers" : [ { "mixer" : "gin", "amount" : "4" },
-		   { "mixer" : "tonic", "amount" : "6" } ] },
+		   { "mixer" : "tonic", "amount" : "16" } ] },
     
     { "_id" : ObjectId("5d0488cb86784f34f7bb9faa"),
       "type" : "drink",
@@ -61,11 +61,23 @@ var data = [
       "mixers" : [ { "mixer" : "vodka", "amount" : "4" },
 		   { "mixer" : "redbull", "amount" : "16" } ] },
     
+    { "_id" : ObjectId("5d0488cb86784f34f7bb9fda"),
+      "type" : "drink",
+      "name" : "Screwdriver", "sysname" : "screwdriver",
+      "mixers" : [ { "mixer" : "vodka", "amount" : "6" },
+		   { "mixer" : "orange", "amount" : "14" } ] },
+    
     { "_id" : ObjectId("5d048c3d86784f34f7bb9fac"),
       "type" : "drink",
       "name" : "Dry martini", "sysname" : "drymartini",
       "mixers" : [ { "mixer" : "gin", "amount" : "6" },
 		   { "mixer" : "martini", "amount" : "4" } ] },
+
+    { "_id" : ObjectId("5d065c3d86784f34f7bb9fac"),
+      "type" : "drink",
+      "name" : "Shit on the grass", "sysname" : "shitonthegrass",
+      "mixers" : [ { "mixer" : "baileys", "amount" : "4" },
+		   { "mixer" : "pisangambon", "amount" : "4" } ] },
     
     { "_id" : ObjectId("5d048e0f86784f34f7bb9fae"),
       "type" : "drink",
@@ -82,32 +94,13 @@ var data = [
 		   { "mixer" : "sprite", "amount" : "12" },
 		   { "mixer" : "grenadine", "amount" : "2" } ] },
 
-    { "_id" : ObjectId("5d04a99186784f34f7bb9fb2"),
+    { "_id" : ObjectId("5d04a54486784f34f7aa5faf"),
       "type" : "drink",
-      "name" : "Dirty Lee", "sysname" : "filthyjohn",
-      "mixers" : [ { "mixer" : "whiskey", "amount" : "4" },
-		   { "mixer" : "redbull", "amount" : "16" } ] },
-
-    { "_id" : ObjectId("5d04aa9886784f34f7bb9fb3"),
-      "type" : "drink",
-      "name" : "Margarita", "sysname" : "margarita",
-      "mixers" : [ { "mixer" : "lime", "amount" : "3" },
-		   { "mixer" : "tequila", "amount" : "4" },
+      "name" : "Cosmopolitan", "sysname" : "cosmopolitan",
+      "mixers" : [ { "mixer" : "cranberry", "amount" : "4" },
+		   { "mixer" : "lime", "amount" : "2" },
+		   { "mixer" : "vodka", "amount" : "4" },
 		   { "mixer" : "cointreau", "amount" : "2" } ] },
-
-    { "_id" : ObjectId("5d04ad4f86784f34f7bb9fb5"),
-      "type" : "drink",
-      "name" : "Aperol spritz", "sysname" : "aperolspritz",
-      "mixers" : [ { "mixer" : "aperol", "amount" : "4" },
-		   { "mixer" : "prosecco", "amount" : "6" },
-		   { "mixer" : "soda", "amount" : "2" } ] },
-
-    { "_id" : ObjectId("5d04adbd86784f34f7bb9fb6"),
-      "type" : "drink",
-      "name" : "Gin rickey", "sysname" : "ginrickey",
-      "mixers" : [ { "mixer" : "gin", "amount" : "6" },
-		   { "mixer" : "lime", "amount" : "3" },
-		   { "mixer" : "soda", "amount" : "11" } ] },
 
     { "_id" : ObjectId("5d04b17986784f34f7bb9fb7"),
       "type" : "drink",
@@ -120,6 +113,25 @@ var data = [
 		   { "mixer" : "tequila", "amount" : "3" },
 		   { "mixer" : "vodka", "amount" : "3" },
 		   { "mixer" : "cointreau", "amount" : "2" } ] },
+    
+    { "_id" : ObjectId("5d04b17986784f34f7bb9fa6"),
+      "type" : "drink",
+      "name" : "Hisingen island iced tea", "sysname" : "hisingenisland",
+      "alcomin" : "1.0", "alcomax" : "10.0",
+      "mixers" : [ { "mixer" : "gin", "amount" : "2" },
+		   { "mixer" : "rom", "amount" : "2" },
+		   { "mixer" : "lemon", "amount" : "2" },
+		   { "mixer" : "tequila", "amount" : "1" },
+		   { "mixer" : "vodka", "amount" : "2" },
+		   { "mixer" : "cointreau", "amount" : "2" },
+		   { "mixer" : "fanta", "amount" : "10" },
+		   { "mixer" : "orange", "amount" : "5" } ] },
+    
+    { "_id" : ObjectId("5d04a99186784f34f7bb9fb2"),
+      "type" : "drink",
+      "name" : "Dirty Lee", "sysname" : "dirtylee",
+      "mixers" : [ { "mixer" : "whiskey", "amount" : "4" },
+		   { "mixer" : "redbull", "amount" : "16" } ] },
 
     { "_id" : ObjectId("5d04b45586784f34f7bb9fb9"),
       "type" : "drink",
@@ -133,7 +145,51 @@ var data = [
       "name" : "Caipirinha", "sysname" : "caipirinha",
       "mixers" : [ { "mixer" : "rom", "amount" : "6" },
 		   { "mixer" : "lime", "amount" : "2" },
-		   { "mixer" : "sugar", "amount" : "3" } ] }
+		   { "mixer" : "sugar", "amount" : "3" } ] },
+    
+    { "_id" : ObjectId("5d04adbd86784f34f7bb9fb6"),
+      "type" : "drink",
+      "name" : "Gin rickey", "sysname" : "ginrickey",
+      "mixers" : [ { "mixer" : "gin", "amount" : "6" },
+		   { "mixer" : "lime", "amount" : "3" },
+		   { "mixer" : "soda", "amount" : "11" } ] },
+
+    { "_id" : ObjectId("5d04ad4f86784f34f7bb9fb5"),
+      "type" : "drink",
+      "name" : "Aperol spritz", "sysname" : "aperolspritz",
+      "mixers" : [ { "mixer" : "aperol", "amount" : "4" },
+		   { "mixer" : "prosecco", "amount" : "6" },
+		   { "mixer" : "soda", "amount" : "2" } ] },
+    
+    { "_id" : ObjectId("5d04aa9886784f34f7bb9fb3"),
+      "type" : "drink",
+      "name" : "Margarita", "sysname" : "margarita",
+      "mixers" : [ { "mixer" : "lime", "amount" : "3" },
+		   { "mixer" : "tequila", "amount" : "4" },
+		   { "mixer" : "cointreau", "amount" : "2" } ] },
+
+    { "_id" : ObjectId("5d04aa9886784f34f7aa9fb3"),
+      "type" : "drink",
+      "name" : "Tequila Sunrise", "sysname" : "tequilasunrise",
+      "mixers" : [ { "mixer" : "tequila", "amount" : "6" },
+		   { "mixer" : "orange", "amount" : "12" },
+		   { "mixer" : "grenadine", "amount" : "2" } ] },
+
+    { "_id" : ObjectId("5d04aa9886784f34f7aa43b3"),
+      "type" : "drink",
+      "name" : "Arizona Sunrise", "sysname" : "arizonasunrise",
+      "mixers" : [ { "mixer" : "tequila", "amount" : "6" },
+		   { "mixer" : "lime", "amount" : "3" },
+		   { "mixer" : "grenadine", "amount" : "2" } ] },
+
+    { "_id" : ObjectId("5d04aa9886784f34f7aa33b3"),
+      "type" : "drink",
+      "name" : "Tom Collins", "sysname" : "tomcollins",
+      "mixers" : [ { "mixer" : "gin", "amount" : "6" },
+		   { "mixer" : "lemon", "amount" : "3" },
+		   { "mixer" : "sugar", "amount" : "1" },
+		   { "mixer" : "soda", "amount" : "11" } ] }
+
 ];
 
 if (process.argv[2] != 'clear') {
